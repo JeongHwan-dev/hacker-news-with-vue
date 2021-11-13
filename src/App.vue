e1b706bdd581daafd300eecc84c27fc5788e27a1
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-13 19:48:49 +0900

    Feat : 네비게이션 구현

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
+    <ToolBar />
     <RouterView />
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+
 export default {
   name: 'App',
+  components: {
+    ToolBar,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <header id="header">
+    <RouterLink to="/news">News</RouterLink> |
+    <RouterLink to="/ask">Ask</RouterLink> |
+    <RouterLink to="/jobs">Jobs</RouterLink> |
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#header {
+  display: flex;
+  padding: 8px;
+  background-color: #42b883;
+  color: #fff;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #35495e;
+    }
+  }
+}
+</style>
