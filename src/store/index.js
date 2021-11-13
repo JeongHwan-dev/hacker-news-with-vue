3f1ea2726b4a902b62c1f046bb4723f83742a668
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-14 00:04:49 +0900

    Feat : store 구현

---
+++ src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    news: [],
+    ask: [],
+    jobs: [],
+  },
+  getters: {
+    fetchedAsk(state) {
+      return state.ask;
+    },
+  },
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    },
+    SET_ASK(state, ask) {
+      state.ask = ask;
+    },
+    SET_JOBS(state, jobs) {
+      state.jobs = jobs;
+    },
+  },
+  actions: {
+    FETCH_NEWS({ commit }) {
+      fetchNewsList()
+        .then(({ data }) => commit('SET_NEWS', data))
+        .catch((error) => console.log(error));
+    },
+    FETCH_ASK({ commit }) {
+      fetchAskList()
+        .then(({ data }) => commit('SET_ASK', data))
+        .catch((error) => console.log(error));
+    },
+    FETCH_JOBS({ commit }) {
+      fetchJobsList()
+        .then(({ data }) => commit('SET_JOBS', data))
+        .catch((error) => console.log(error));
+    },
+  },
+});
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <div v-for="item in ask" :key="item.id">
+    <div v-for="item in fetchedAsk" :key="item.id">
       {{ item.title }}
     </div>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
+import { mapGetters } from 'vuex';
 
 export default {
-  data() {
-    return {
-      ask: [],
-    };
+  computed: {
+    ...mapGetters(['fetchedAsk']),
   },
   created() {
-    fetchAskList()
-      .then((response) => (this.ask = response.data))
-      .catch((error) => console.log(error));
+    this.$store.dispatch('FETCH_ASK');
   },
 };
 </script>
