695f1f780c89227022856726c2c2863b58d5811b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-15 16:47:19 +0900

    Revise : 'fontAwesome.js'파일 디렉터리 변경

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import { router } from './routes/index.js';
 import { store } from './store/index.js';
-import FontAwesomeIcon from './assets/fontAwesome.js';
+import FontAwesomeIcon from './plugins/fontAwesome.js';
 
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
 Vue.config.productionTip = false;
