a3fa8a0780fd826b265811c8577a6c37e60b08dc
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-15 17:21:44 +0900

    Design : 헤더 디자인 수정

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,8 +1,11 @@
 <template>
   <header id="header">
-    <RouterLink to="/news" class="header__link">News</RouterLink>|
-    <RouterLink to="/ask" class="header__link">Ask</RouterLink>|
-    <RouterLink to="/jobs" class="header__link">Jobs</RouterLink>
+    <div class="header__logo">Y</div>
+    <nav class="header__nav">
+      <RouterLink to="/news" class="header__link">News</RouterLink>
+      <RouterLink to="/ask" class="header__link">Ask</RouterLink>
+      <RouterLink to="/jobs" class="header__link">Jobs</RouterLink>
+    </nav>
   </header>
 </template>
 
@@ -11,17 +14,39 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+$logo-length: 40px;
+
 #header {
   display: flex;
-  padding: 8px;
+  justify-content: space-between;
+  padding: 10px 24px;
   background-color: #42b883;
   color: #fff;
 
-  .header__link {
-    color: #fff;
+  .header__logo {
+    width: $logo-length;
+    height: $logo-length;
+    border: 1.5px solid #fff;
+    box-sizing: border-box;
+    font-size: 26px;
+    font-weight: 700;
+    text-align: center;
+  }
 
-    &.router-link-exact-active {
-      color: #35495e;
+  .header__nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .header__link {
+      padding: 0 12px;
+      font-size: 18px;
+      font-weight: 500;
+      color: #fff;
+
+      &.router-link-exact-active {
+        color: #35495e;
+      }
     }
   }
 }
